7f91ab0e6e0187d78d171ccf75e13d326e4e6eac
Dali47 <meftah.dali@gmail.com>
2022-05-06 02:29:21 +0100

    make it better

--- transformation/models.py
+++ transformation/models.py
@@ -9,6 +9,6 @@ class transformation(models.Model):
     Week_Goal = models.CharField(max_length=512,default=False)
     Body_Height = models.FloatField(default=False)
     Body_Weight = models.FloatField(default=False)
-    Image_Link = models.URLField(blank=False)
+    Image_Link = models.CharField(max_length=512,default=False)
     def __str__(self):
         return self.Member_Email
