efd79ca28960b9f13baccd94070763714c0d0305
Dali47 <meftah.dali@gmail.com>
2022-05-16 23:46:07 +0100

    Fixing data base lik

--- transformation/views.py
+++ transformation/views.py
@@ -12,7 +12,6 @@ from .serializers import  TransformationSerializer
 #12
 class TransformationList(APIView):
         serializer_class  = TransformationSerializer
-        lookup_field = 'Member_Email'
 
         def get_queryset(self):
             Transformations = transformation.objects.all()
@@ -27,5 +26,15 @@ class TransformationList(APIView):
 
             return Response(serializer.data)   
 
-        def post(self):
-            pass
+        def post(self, request, *args, **kwargs):
+            transformation_data = request.data
+
+            new_trans = transformation.objects.create(Member_Email=transformation_data["Member_Email"], Week_Date=transformation_data[
+                "Week_Date"], Week_Goal=transformation_data["Week_Goal"], Body_Height=transformation_data["Body_Height"], Body_Weight=transformation_data["Body_Weight"]
+                , Image_Link=transformation_data["Image_Link"])
+
+            new_trans.save()
+
+            serializer = TransformationSerializer(new_trans)
+
+            return Response(serializer.data)
